29a5f5e98e5efba080951e1b7a0283806eb00cbe
K4rLiToX <48551464+K4rLiToX@users.noreply.github.com>
2020-11-24 23:21:13 +0100

    Select MealType InputDropdown
    Intento por ahora fallido de hacer un dropdown select para elegir el mealtype. Se supone que el código está bien si estuviera en un activity. Hay que mirar cómo hacerlo en un fragment

--- app/src/main/java/es/android/dacooker/fragments/AddRecipeFragment.java
+++ app/src/main/java/es/android/dacooker/fragments/AddRecipeFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import es.android.dacooker.R;
 
@@ -14,6 +16,10 @@ import es.android.dacooker.R;
  * A simple {@link Fragment} subclass.
  */
 public class AddRecipeFragment extends Fragment {
+
+    String[] MEALTYPES = new String[]{"Launch", "Dinner", "Other"};
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.meal_type_dropdown_item, MEALTYPES);
+    AutoCompleteTextView mealTypeDropdown = getView().findViewById(R.id.recipe_mealType_dropdown_select);
 
     public AddRecipeFragment() {
         // Required empty public constructor
@@ -23,6 +29,7 @@ public class AddRecipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mealTypeDropdown.setAdapter(adapter);
         return inflater.inflate(R.layout.fragment_add_recipe, container, false);
     }
 }
