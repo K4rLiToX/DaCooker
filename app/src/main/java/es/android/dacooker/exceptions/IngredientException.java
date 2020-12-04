5f682f749c150897f1354a9ef150422ca1b31133
K4rLiToX <48551464+K4rLiToX@users.noreply.github.com>
2020-12-04 23:10:55 +0100

    Paquete Exceptions comentado

--- app/src/main/java/es/android/dacooker/exceptions/IngredientException.java
+++ app/src/main/java/es/android/dacooker/exceptions/IngredientException.java
@@ -2,10 +2,12 @@ package es.android.dacooker.exceptions;
 
 public class IngredientException extends Exception{
 
+    //Constructor
     public IngredientException(){
 
     }
 
+    //Método que muestra el mensaje pasado por parámetro cuando se ejecuta la excepción
     public IngredientException(String msg){
         super(msg);
     }
--- app/src/main/java/es/android/dacooker/exceptions/StepException.java
+++ app/src/main/java/es/android/dacooker/exceptions/StepException.java
@@ -2,10 +2,12 @@ package es.android.dacooker.exceptions;
 
 public class StepException extends Exception {
 
+    //Constructor
     public StepException(){
 
     }
 
+    //Método que muestra el mensaje pasado por parámetro cuando se ejecuta la excepción
     public StepException(String msg){
         super(msg);
     }
