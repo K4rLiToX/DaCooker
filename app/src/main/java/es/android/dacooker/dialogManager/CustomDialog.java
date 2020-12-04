f4958aecf7c49a164a297186addfa853e272e47d
K4rLiToX <48551464+K4rLiToX@users.noreply.github.com>
2020-12-04 23:07:16 +0100

    CustomDialog comentado y organizado

--- app/src/main/java/es/android/dacooker/dialogManager/CustomDialog.java
+++ app/src/main/java/es/android/dacooker/dialogManager/CustomDialog.java
@@ -34,7 +34,7 @@ public class CustomDialog extends DialogFragment {
     //TAG
     private static final String TAG = "CustomDialog";
 
-    //Views
+    //Vistas
     RadioGroup radioGroup;
     LinearLayout search_dialog_time_layout;
     TextInputEditText input_hours, input_minutes;
@@ -42,20 +42,23 @@ public class CustomDialog extends DialogFragment {
     AutoCompleteTextView selectMealType;
     Button btnSearch, btnDismiss;
 
-    //Mealtype Adapters
+    //Adaptador de la enumeración MealType
     MealType[] MEALTYPES = MealType.values();
     ArrayAdapter<MealType> adapter;
 
-    //Interface
+    //Interfaz que delega las funcionalidades necesarias a la activity main
     private OnDialogInputListener onDialogInputListener;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //Inflo la vista del dialogo personalizado
         View view = inflater.inflate(R.layout.search_dialog, container, false);
-
+        //Creo el adaptador
         adapter =  new ArrayAdapter<>(getActivity(), R.layout.meal_type_dropdown_item, MEALTYPES);
+        //Inicializo las vistas necesarias
         this.selectMealType = view.findViewById(R.id.search_dialog_select_mealType);
+        //Seteo el adapter al select de mealtype
         this.selectMealType.setAdapter(adapter);
 
         this.radioGroup = view.findViewById(R.id.search_dialog_radio_group);
@@ -67,100 +70,130 @@ public class CustomDialog extends DialogFragment {
         this.btnSearch = view.findViewById(R.id.search_dialog_btn_search);
         this.btnDismiss = view.findViewById(R.id.search_dialog_btn_dismiss);
 
+        //Añado funcionalidad al grupo de radio buttons
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
+            //Se ejecuta cuando se pulsa algunos de los radio buttons del grupo
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 changeSelection(radioGroup, i);
             }
         });
 
+        //Añado funcionalidad al botón de confirmación del diálogo personalizado
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Cuando se pulsa se filtra la lista de recetas de acuerdo a los filtros seleccionados
             public void onClick(View view) {
                 filterSearch();
             }
         });
-
+        //Añado funcionalidad al botón cancelar del diálogo personalizado
         btnDismiss.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Cuando se pulsa cierro el diálogo personalizado
             public void onClick(View view) {
                dismiss();
             }
         });
-
-
         return view;
     }
 
+    /*Métodos del diálogo personalizado*/
+
+    //Cambia la vista del diálogo personalizad en función del radio button que se seleccione
     private void changeSelection(RadioGroup radioGroup, int i){
-        if(i == R.id.search_dialog_radio_mealType){
+        if(i == R.id.search_dialog_radio_mealType){ //Si se pulsa el radio button de Mealtype
+            //Oculto el layout del tiempo
             search_dialog_time_layout.setVisibility(View.GONE);
+            //Hago visible el select de mealtype
             search_dialog_mealType_layout.setVisibility(View.VISIBLE);
-        } else if(i == R.id.search_dialog_radio_time){
+        } else if(i == R.id.search_dialog_radio_time){ //Si se pulsa el radio button de Tiempo
+            //Hago visible el layout del tiempo
             search_dialog_time_layout.setVisibility(View.VISIBLE);
+            //Oculto el select de mealtype
             search_dialog_mealType_layout.setVisibility(View.GONE);
-        } else {
+        } else { //En caso de que ninguno esté seleccionado oculto las dos vistas
             search_dialog_time_layout.setVisibility(View.GONE);
             search_dialog_mealType_layout.setVisibility(View.GONE);
         }
     }
 
+    //Realiza el filtro de recetas según el radio button seleccionado
     private void filterSearch(){
+        //Obtengo el id del radio button seleccionado
         int radioChecked = radioGroup.getCheckedRadioButtonId();
 
-        if(radioChecked == R.id.search_dialog_radio_mealType){
+        if(radioChecked == R.id.search_dialog_radio_mealType){ //Si está seleccionado el radio button de Mealtype
+            //Obtendo el valor del select
             String meal = selectMealType.getText().toString();
 
-            if(validateMealType(meal)){
+            if(validateMealType(meal)){ //Si el valor es válido
                 BBDD_Helper db = new BBDD_Helper(getActivity());
+                //Creo la lista resultado de recetas
                 List<RecipeModel> resultList;
                 try {
+                    //Obtengo las recetas filtradas por el tipo de comida
                     resultList = BD_Operations.getRecipesByMealType(MealType.valueOf(meal), db);
-                    if(resultList.isEmpty()) Toast.makeText(getActivity(),
-                            getString(R.string.search_dialog_err_no_meal_results) + meal, Toast.LENGTH_SHORT).show();
+                    //Si la lista es vacía mando mensaje de que no hay ninguna receta con ese tipo de comida
+                    //En caso contrario envío la lista resultado junto con el filtro 1 (mealtype) y el tipo de comida a la actividad principal mediante la interfaz
+                    if(resultList.isEmpty()) Toast.makeText(getActivity(),getString(R.string.search_dialog_err_no_meal_results) + meal, Toast.LENGTH_SHORT).show();
                     else onDialogInputListener.sendResultList(resultList, "1", meal);
                 } catch (Exception e){
+                    //En caso de que ocurra algún error envío mensaje notificando
                     Toast.makeText(getActivity(), getString(R.string.search_dialog_err_retrieving), Toast.LENGTH_SHORT).show();
                 }
+                //Por último, pase lo que pase, cierro el diálogo personalizado
                 dismiss();
-            } else {
+            } else { //Si el valor obtenido del select no es válido significa que no se ha seleccionado ninguna opción
+                //Envió mensaje pidiendo que se seleccione una opción
                 Toast.makeText(getActivity(), R.string.search_dialog_mealType_select_error, Toast.LENGTH_LONG).show();
             }
-        } else if(radioChecked == R.id.search_dialog_radio_time){
+        } else if(radioChecked == R.id.search_dialog_radio_time){ // Si está seleccionado el radio button de Tiempo
+            //Obtengo los datos de los inputs
             String hour = input_hours.getText().toString().trim();
             String minute = input_minutes.getText().toString().trim();
 
-            if(validateTime(hour, minute)){
+            if(validateTime(hour, minute)){ //Si los datos son válidos
                 BBDD_Helper db = new BBDD_Helper(getActivity());
+                //Creo la lista resultado
                 List<RecipeModel> resultList;
                 try {
+                    //Obtengo las recetas que tengan un tiempo menor al que me han dado en los inputs
                     resultList = BD_Operations.getRecipesByLessExecutionTime(Integer.parseInt(hour), Integer.parseInt(minute), db);
+                    //Si la lista resultado está vacía mando un mensaje notificando que no hay recetas que cumplan con esa propiedad
+                    //En caso contrario envío la lista resultado junto con el filtro 3 (tiempo) y una cadena de texto conteniendo las horas y los minutos obtenidos de los inputs
                     if(resultList.isEmpty()) Toast.makeText(getActivity(), getString(R.string.search_dialog_no_recipes_matching), Toast.LENGTH_SHORT).show();
                     else onDialogInputListener.sendResultList(resultList, "2", hour+":"+minute);
                 } catch (Exception e){
+                    //En caso de error notifico
                     Toast.makeText(getActivity(), getString(R.string.search_dialog_err_retrieving), Toast.LENGTH_SHORT).show();
                 }
+                //Por último, pase lo que pase, cierro el diálogo personalizado
                 dismiss();
-            } else Toast.makeText(getActivity(), R.string.search_dialog_time_inputs_error, Toast.LENGTH_LONG).show();
-        } else Toast.makeText(getActivity(), R.string.search_dialog_no_radio_selected, Toast.LENGTH_SHORT).show();
+            } else Toast.makeText(getActivity(), R.string.search_dialog_time_inputs_error, Toast.LENGTH_LONG).show(); //Si los valores de tiempo obtenidos de los inputs no son válidos envío mensaje pidiendo que se escriban valores correctos
+        } else Toast.makeText(getActivity(), R.string.search_dialog_no_radio_selected, Toast.LENGTH_SHORT).show(); //Si no hay ningún radio button seleccionado envío mensaje pidiendo que se seleccione una opción
     }
 
+    //Valida que el tipo de comida sea válido, es decir, que no sea vacío (no se selcciona opción)
     private boolean validateMealType(String mealType){
         return !mealType.isEmpty();
     }
 
+    //Valida que los valores de horas y minutos sean válidos, es decir, que ningún campo esté vacio, que las horas y los minutos no sean negativos y que los minutos no sean superiores a 59
     private boolean validateTime(String hour, String minute){
         return !hour.isEmpty() && !minute.isEmpty() && Integer.parseInt(hour) >= 0
                 && Integer.parseInt(minute) >= 0 && Integer.parseInt(minute) <= 59;
     }
 
     @Override
+    //Método necesario para utilizar una interfaz en el diálogo personalizado
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         try { onDialogInputListener = (OnDialogInputListener) getActivity();
         } catch (ClassCastException e){ Log.e(TAG, "onAttach: ClassCastException: " + e.getMessage()); }
     }
 
+    //Creación de la interfaz utilizada junto con el método y los parámetros que necesita
     public interface OnDialogInputListener {
         void sendResultList(List<RecipeModel> resultList, String filter, String search);
     }
